b3fd790da4a87ee70e1811defaabdb804a330267
mitch <miche.v.b@gmail.com>
2022-03-09 09:55:39 -0600

    some updates

---
+++ aux_functions.jl
@@ -0,0 +1,44 @@
+# Packages for plotting and saving/loading data 
+using NPZ, Plots
+
+include("functions.jl")
+
+# Plot style
+begin
+    theme(:juno); #:vibrant
+    default(fontfamily="Computer Modern", framestyle=:box); # LaTex-style
+    gr(size = (400, 400)); # default plot size
+end
+
+# This function to save data to files
+function save_data(res, path, suffix)
+    npzwrite(path*"U"*suffix*".npz", res.val_fun.U)
+    npzwrite(path*"W_L"*suffix*".npz", res.val_fun.W_L)
+    npzwrite(path*"W_H"*suffix*".npz", res.val_fun.W_H)
+    npzwrite(path*"k_pol_U"*suffix*".npz", res.k_pol.U)
+    npzwrite(path*"s_pol_U"*suffix*".npz", res.s_pol.U)
+    npzwrite(path*"k_pol_WL"*suffix*".npz", res.k_pol.W_L)
+    npzwrite(path*"s_pol_WL"*suffix*".npz", res.s_pol.W_L)
+    npzwrite(path*"k_pol_WH"*suffix*".npz", res.k_pol.W_H)
+    npzwrite(path*"s_pol_WH"*suffix*".npz", res.s_pol.W_H)
+end
+
+# This function read data from files and returns all the structures needed
+
+function read_data(path, suffix)
+
+    # First initialize the structures
+    prim, res, pre_comp = Init()
+
+    res.val_fun.U = npzread(path*"U"*suffix*".npz")
+    res.val_fun.W_L = npzread(path*"W_L"*suffix*".npz")
+    res.val_fun.W_H = npzread(path*"W_H"*suffix*".npz")
+    res.k_pol.U = npzread(path*"k_pol_U"*suffix*".npz"  )
+    res.s_pol.U = npzread(path*"s_pol_U"*suffix*".npz"  )
+    res.k_pol.W_L = npzread(path*"k_pol_WL"*suffix*".npz")
+    res.s_pol.W_L = npzread(path*"s_pol_WL"*suffix*".npz")
+    res.k_pol.W_H = npzread(path*"k_pol_WH"*suffix*".npz")
+    res.s_pol.W_H = npzread(path*"s_pol_WH"*suffix*".npz")
+
+    return prim, res, pre_comp
+end
--- runfile.jl
+++ runfile.jl
@@ -1,142 +1,55 @@
 # Load packages
 using Parameters, Statistics, Distributions, ProgressBars, SharedArrays, Distributed
-using Plots, JLD, NPZ
-
 addprocs(6) 
 
-# Plot style
-begin
-    theme(:juno); #:vibrant
-    default(fontfamily="Computer Modern", framestyle=:box); # LaTex-style
-    gr(size = (400, 400)); # default plot size
-end
-res_v2 = load("Saved Results/results_v2.jld")
-
 @everywhere include("functions.jl")
+include("aux_functions.jl")
+
+path = "Saved Results/"
+
+begin 
+    @time prim, res, pre_comp = Init();
+    # @time vfn(prim, res, pre_comp)
+    # save_data(res, path, "_v2")
+    # using JLD
+
+    # jldopen("results.jld", "w") do file
+    #     write(file, "val_fun", res.val_fun)
+    #     write(file, "k_pol", res.k_pol)
+    #     write(file, "s_pol", res.s_pol)
+    # end
+    # prim, res, pre_comp = Init()
+    # vfn(prim,res,pre_comp)
+
+    # prim, res_vmu, pre_comp = Init(μarg = 0.95)
+    # vfn(prim,res_vmu,pre_comp)
+    # save_data(res, path)
+
+        
+
+    # prim, res_vA, pre_comp = Init(Aarg = 1.05)
+    # vfn(prim,res_vA,pre_comp)
+    # save_data(res, path)
 
 
-# @time prim, res, pre_comp = Init();
-# @time vfn(prim, res, pre_comp)
+    # prim, res_vthr, pre_comp = Init(thr = 0.5)
+    # vfn(prim,res_vthr,pre_comp)
+    # save_data(res, path)
+end 
 
-# using JLD
-
-# jldopen("results.jld", "w") do file
-#     write(file, "val_fun", res.val_fun)
-#     write(file, "k_pol", res.k_pol)
-#     write(file, "s_pol", res.s_pol)
-# end
-prim, res, pre_comp = Init()
-vfn(prim,res,pre_comp)
-begin
-    npzwrite("Saved Results/U_v2.npz", res.val_fun.U)
-    npzwrite("Saved Results/W_L_v2.npz", res.val_fun.W_L)
-    npzwrite("Saved Results/W_H_v2.npz", res.val_fun.W_H)
-    npzwrite("Saved Results/k_pol_U_v2.npz", res.k_pol.U)
-    npzwrite("Saved Results/s_pol_U_v2.npz", res.s_pol.U)
-    # npzwrite("Saved Results/k_pol_ind_U1.npz", res.k_pol.ind_U)
-    # npzwrite("Saved Results/s_pol_ind_U1.npz", res.s_pol.ind_U)
-    npzwrite("Saved Results/k_pol_WL_v2.npz", res.k_pol.W_L)
-    npzwrite("Saved Results/s_pol_WL_v2.npz", res.s_pol.W_L)
-    # npzwrite("Saved Results/k_pol_ind_WL1.npz", res.k_pol_ind_W_L)
-    # npzwrite("Saved Results/s_pol_ind_WL1.npz", res.s_pol_ind_W_L)
-    npzwrite("Saved Results/k_pol_WH_v2.npz", res.k_pol.W_H)
-    npzwrite("Saved Results/s_pol_WH_v2.npz", res.s_pol.W_H)
-    # npzwrite("Saved Results/k_pol_ind_WH1.npz", res.k_pol_ind_W_H)
-    # npzwrite("Saved Results/s_pol_ind_WH1.npz", res.s_pol_ind_W_H)
-end
-
-prim, res_vmu, pre_comp = Init(μarg = 0.95)
-vfn(prim,res_vmu,pre_comp)
-begin
-    npzwrite("Saved Results/U_vmu.npz", res_vmu.val_fun.U)
-    npzwrite("Saved Results/W_L_vmu.npz", res_vmu.val_fun.W_L)
-    npzwrite("Saved Results/W_H_vmu.npz", res_vmu.val_fun.W_H)
-    npzwrite("Saved Results/k_pol_U_vmu.npz", res_vmu.k_pol.U)
-    npzwrite("Saved Results/s_pol_U_vmu.npz", res_vmu.s_pol.U)
-    # npzwrite("Saved Results/k_pol_ind_vmu.npz", res_vmu.k_pol.ind_U)
-    # npzwrite("Saved Results/s_pol_ind_vmu.npz", res_vmu.s_pol.ind_U)
-    npzwrite("Saved Results/k_pol_WL_vmu.npz", res_vmu.k_pol.W_L)
-    npzwrite("Saved Results/s_pol_WL_vmu.npz", res_vmu.s_pol.W_L)
-    # npzwrite("Saved Results/k_pol_ind_vmu.npz", res_vmu.k_pol_ind_W_L)
-    # npzwrite("Saved Results/s_pol_ind_vmu.npz", res_vmu.s_pol_ind_W_L)
-    npzwrite("Saved Results/k_pol_WH_vmu.npz", res_vmu.k_pol.W_H)
-    npzwrite("Saved Results/s_pol_WH_vmu.npz", res_vmu.s_pol.W_H)
-    # npzwrite("Saved Results/k_pol_ind_vmu.npz", res_vmu.k_pol_ind_W_H)
-    # npzwrite("Saved Results/s_pol_ind_vmu.npz", res_vmu.s_pol_ind_W_H)
-end
-
-prim, res_vA, pre_comp = Init(Aarg = 1.05)
-vfn(prim,res_vA,pre_comp)
-begin
-    npzwrite("Saved Results/U_vA.npz", res_vA.val_fun.U)
-    npzwrite("Saved Results/W_L_vA.npz", res_vA.val_fun.W_L)
-    npzwrite("Saved Results/W_H_vA.npz", res_vA.val_fun.W_H)
-    npzwrite("Saved Results/k_pol_U_vA.npz", res_vA.k_pol.U)
-    npzwrite("Saved Results/s_pol_U_vA.npz", res_vA.s_pol.U)
-    # npzwrite("Saved Results/k_pol_ind_vA.npz", res_vA.k_pol.ind_U)
-    # npzwrite("Saved Results/s_pol_ind_vA.npz", res_vA.s_pol.ind_U)
-    npzwrite("Saved Results/k_pol_WL_vA.npz", res_vA.k_pol.W_L)
-    npzwrite("Saved Results/s_pol_WL_vA.npz", res_vA.s_pol.W_L)
-    # npzwrite("Saved Results/k_pol_ind_vA.npz", res_vA.k_pol_ind_W_L)
-    # npzwrite("Saved Results/s_pol_ind_vA.npz", res_vA.s_pol_ind_W_L)
-    npzwrite("Saved Results/k_pol_WH_vA.npz", res_vA.k_pol.W_H)
-    npzwrite("Saved Results/s_pol_WH_vA.npz", res_vA.s_pol.W_H)
-    # npzwrite("Saved Results/k_pol_ind_vA.npz", res_vA.k_pol_ind_W_H)
-    # npzwrite("Saved Results/s_pol_ind_vA.npz", res_vA.s_pol_ind_W_H)
-end
-
-
-prim, res_vthr, pre_comp = Init(thr = 0.5)
-vfn(prim,res_vthr,pre_comp)
-begin
-    npzwrite("Saved Results/U_vthr.npz", res_vthr.val_fun.U)
-    npzwrite("Saved Results/W_L_vthr.npz", res_vthr.val_fun.W_L)
-    npzwrite("Saved Results/W_H_vthr.npz", res_vthr.val_fun.W_H)
-    npzwrite("Saved Results/k_pol_U_vthr.npz", res_vthr.k_pol.U)
-    npzwrite("Saved Results/s_pol_U_vthr.npz", res_vthr.s_pol.U)
-    # npzwrite("Saved Results/k_pol_ind_vthr.npz", res_vthr.k_pol.ind_U)
-    # npzwrite("Saved Results/s_pol_ind_vthr.npz", res_vthr.s_pol.ind_U)
-    npzwrite("Saved Results/k_pol_WL_vthr.npz", res_vthr.k_pol.W_L)
-    npzwrite("Saved Results/s_pol_WL_vthr.npz", res_vthr.s_pol.W_L)
-    # npzwrite("Saved Results/k_pol_ind_vthr.npz", res_vthr.k_pol_ind_W_L)
-    # npzwrite("Saved Results/s_pol_ind_vthr.npz", res_vthr.s_pol_ind_W_L)
-    npzwrite("Saved Results/k_pol_WH_vthr.npz", res_vthr.k_pol.W_H)
-    npzwrite("Saved Results/s_pol_WH_vthr.npz", res_vthr.s_pol.W_H)
-    # npzwrite("Saved Results/k_pol_ind_vthr.npz", res_vthr.k_pol_ind_W_H)
-    # npzwrite("Saved Results/s_pol_ind_vthr.npz", res_vthr.s_pol_ind_W_H)
-end
-
-begin
-    res.val_fun.U = npzread("Saved Results/U_v2.npz")
-    res.val_fun.W_L = npzread("Saved Results/W_L_v2.npz")
-    res.val_fun.W_H = npzread("Saved Results/W_H_v2.npz")
-    res.k_pol.U = npzread("Saved Results/k_pol_U_v2.npz"  )
-    res.s_pol.U = npzread("Saved Results/s_pol_U_v2.npz"  )
-    # res.k_pol.ind_U = npzread("Saved Results/k_pol_ind_U1.npz")
-    # res.s_pol.ind_U = npzread("Saved Results/s_pol_ind_U1.npz")
-    res.k_pol.W_L = npzread("Saved Results/k_pol_WL_v2.npz")
-    res.s_pol.W_L = npzread("Saved Results/s_pol_WL_v2.npz")
-    # res.k_pol_ind_W_L = npzread("Saved Results/k_pol_ind_WL1.npz")
-    # res.s_pol_ind_W_L = npzread("Saved Results/s_pol_ind_WL1.npz")
-    res.k_pol.W_H = npzread("Saved Results/k_pol_WH_v2.npz")
-    res.s_pol.W_H = npzread("Saved Results/s_pol_WH_v2.npz")
-    # res.k_pol_ind_W_H = npzread("Saved Results/k_pol_ind_WH1.npz")
-    # res.s_pol_ind_W_H = npzread("Saved Results/s_pol_ind_WH1.npz")
-end
+# prim, res, pre_comp = read_data(path, "_vA")
 
 sim = Init2(prim)
 runsim(prim, res, sim, pre_comp)
-# res_v2 = Results(res_v2["val_fun"], res_v2["k_pol"], res_v2["s_pol"])
-res_v2 = res
 
-plot(res_v2.val_fun.U[:, 50, 56, 12])
-plot!(res_v2["val_fun"].W_H[:, 50, 56, 12])
-plot!(res_v2["val_fun"].W_L[:, 50, 56, 12])
+# plot(res_v2.val_fun.U[:, 50, 56, 12])
+# plot!(res_v2["val_fun"].W_H[:, 50, 56, 12])
+# plot!(res_v2["val_fun"].W_L[:, 50, 56, 12])
 
-plot(res_v2["s_pol"].U[20,40,1,:], label="1")
-plot!(res_v2["s_pol"].U[20,40,10,:], label="10")
-plot!(res_v2["s_pol"].U[20,40,20,:], label="20")
-plot!(res_v2["s_pol"].U[20,40,30,:], label="30")
-plot(res_v2["s_pol"].U[20,40,100,:], label="100")
+# plot(res_v2["s_pol"].U[20,40,1,:], label="1")
+# plot!(res_v2["s_pol"].U[20,40,10,:], label="10")
+# plot!(res_v2["s_pol"].U[20,40,20,:], label="20")
+# plot!(res_v2["s_pol"].U[20,40,30,:], label="30")
+# plot(res_v2["s_pol"].U[20,40,100,:], label="100")
 
-res_v2["s_pol"].U[:,:,1,:]
+# res_v2["s_pol"].U[:,:,1,:]
