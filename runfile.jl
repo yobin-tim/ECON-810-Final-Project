8470802532f51694efcf57964e1c8a0dccac93cd
yobin-tim <timilsena.yobin@gmail.com>
2022-03-09 01:03:06 -0600

    runfile minor update

--- runfile.jl
+++ runfile.jl
@@ -2,7 +2,7 @@
 using Parameters, Statistics, Distributions, ProgressBars, SharedArrays, Distributed
 using Plots, JLD, NPZ
 
-# addprocs(6) 
+addprocs(6) 
 
 # Plot style
 begin
@@ -12,7 +12,7 @@ begin
 end
 res_v2 = load("Saved Results/results_v2.jld")
 
-include("functions.jl")
+@everywhere include("functions.jl")
 
 
 # @time prim, res, pre_comp = Init();
@@ -25,9 +25,8 @@ include("functions.jl")
 #     write(file, "k_pol", res.k_pol)
 #     write(file, "s_pol", res.s_pol)
 # end
-
 prim, res, pre_comp = Init()
-
+vfn(prim,res,pre_comp)
 begin
     npzwrite("Saved Results/U_v2.npz", res.val_fun.U)
     npzwrite("Saved Results/W_L_v2.npz", res.val_fun.W_L)
@@ -44,6 +43,67 @@ begin
     npzwrite("Saved Results/s_pol_WH_v2.npz", res.s_pol.W_H)
     # npzwrite("Saved Results/k_pol_ind_WH1.npz", res.k_pol_ind_W_H)
     # npzwrite("Saved Results/s_pol_ind_WH1.npz", res.s_pol_ind_W_H)
+end
+
+prim, res_vmu, pre_comp = Init(μarg = 0.95)
+vfn(prim,res_vmu,pre_comp)
+begin
+    npzwrite("Saved Results/U_vmu.npz", res_vmu.val_fun.U)
+    npzwrite("Saved Results/W_L_vmu.npz", res_vmu.val_fun.W_L)
+    npzwrite("Saved Results/W_H_vmu.npz", res_vmu.val_fun.W_H)
+    npzwrite("Saved Results/k_pol_U_vmu.npz", res_vmu.k_pol.U)
+    npzwrite("Saved Results/s_pol_U_vmu.npz", res_vmu.s_pol.U)
+    # npzwrite("Saved Results/k_pol_ind_vmu.npz", res_vmu.k_pol.ind_U)
+    # npzwrite("Saved Results/s_pol_ind_vmu.npz", res_vmu.s_pol.ind_U)
+    npzwrite("Saved Results/k_pol_WL_vmu.npz", res_vmu.k_pol.W_L)
+    npzwrite("Saved Results/s_pol_WL_vmu.npz", res_vmu.s_pol.W_L)
+    # npzwrite("Saved Results/k_pol_ind_vmu.npz", res_vmu.k_pol_ind_W_L)
+    # npzwrite("Saved Results/s_pol_ind_vmu.npz", res_vmu.s_pol_ind_W_L)
+    npzwrite("Saved Results/k_pol_WH_vmu.npz", res_vmu.k_pol.W_H)
+    npzwrite("Saved Results/s_pol_WH_vmu.npz", res_vmu.s_pol.W_H)
+    # npzwrite("Saved Results/k_pol_ind_vmu.npz", res_vmu.k_pol_ind_W_H)
+    # npzwrite("Saved Results/s_pol_ind_vmu.npz", res_vmu.s_pol_ind_W_H)
+end
+
+prim, res_vA, pre_comp = Init(Aarg = 1.05)
+vfn(prim,res_vA,pre_comp)
+begin
+    npzwrite("Saved Results/U_vA.npz", res_vA.val_fun.U)
+    npzwrite("Saved Results/W_L_vA.npz", res_vA.val_fun.W_L)
+    npzwrite("Saved Results/W_H_vA.npz", res_vA.val_fun.W_H)
+    npzwrite("Saved Results/k_pol_U_vA.npz", res_vA.k_pol.U)
+    npzwrite("Saved Results/s_pol_U_vA.npz", res_vA.s_pol.U)
+    # npzwrite("Saved Results/k_pol_ind_vA.npz", res_vA.k_pol.ind_U)
+    # npzwrite("Saved Results/s_pol_ind_vA.npz", res_vA.s_pol.ind_U)
+    npzwrite("Saved Results/k_pol_WL_vA.npz", res_vA.k_pol.W_L)
+    npzwrite("Saved Results/s_pol_WL_vA.npz", res_vA.s_pol.W_L)
+    # npzwrite("Saved Results/k_pol_ind_vA.npz", res_vA.k_pol_ind_W_L)
+    # npzwrite("Saved Results/s_pol_ind_vA.npz", res_vA.s_pol_ind_W_L)
+    npzwrite("Saved Results/k_pol_WH_vA.npz", res_vA.k_pol.W_H)
+    npzwrite("Saved Results/s_pol_WH_vA.npz", res_vA.s_pol.W_H)
+    # npzwrite("Saved Results/k_pol_ind_vA.npz", res_vA.k_pol_ind_W_H)
+    # npzwrite("Saved Results/s_pol_ind_vA.npz", res_vA.s_pol_ind_W_H)
+end
+
+
+prim, res_vthr, pre_comp = Init(thr = 0.5)
+vfn(prim,res_vthr,pre_comp)
+begin
+    npzwrite("Saved Results/U_vthr.npz", res_vthr.val_fun.U)
+    npzwrite("Saved Results/W_L_vthr.npz", res_vthr.val_fun.W_L)
+    npzwrite("Saved Results/W_H_vthr.npz", res_vthr.val_fun.W_H)
+    npzwrite("Saved Results/k_pol_U_vthr.npz", res_vthr.k_pol.U)
+    npzwrite("Saved Results/s_pol_U_vthr.npz", res_vthr.s_pol.U)
+    # npzwrite("Saved Results/k_pol_ind_vthr.npz", res_vthr.k_pol.ind_U)
+    # npzwrite("Saved Results/s_pol_ind_vthr.npz", res_vthr.s_pol.ind_U)
+    npzwrite("Saved Results/k_pol_WL_vthr.npz", res_vthr.k_pol.W_L)
+    npzwrite("Saved Results/s_pol_WL_vthr.npz", res_vthr.s_pol.W_L)
+    # npzwrite("Saved Results/k_pol_ind_vthr.npz", res_vthr.k_pol_ind_W_L)
+    # npzwrite("Saved Results/s_pol_ind_vthr.npz", res_vthr.s_pol_ind_W_L)
+    npzwrite("Saved Results/k_pol_WH_vthr.npz", res_vthr.k_pol.W_H)
+    npzwrite("Saved Results/s_pol_WH_vthr.npz", res_vthr.s_pol.W_H)
+    # npzwrite("Saved Results/k_pol_ind_vthr.npz", res_vthr.k_pol_ind_W_H)
+    # npzwrite("Saved Results/s_pol_ind_vthr.npz", res_vthr.s_pol_ind_W_H)
 end
 
 begin
